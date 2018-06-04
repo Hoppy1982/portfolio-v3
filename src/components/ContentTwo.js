03b9c30b5be7590d84a8e2838a1a970f9231f094
mark hopcraft <mark82hopcraft@gmail.com>
2018-06-04 17:51:23 +0100

    fetches for indiv data grouped with 2 levels of promise.all. I don't eve understand what I'm saying at this point

--- src/components/ContentTwo.js
+++ src/components/ContentTwo.js
@@ -16,7 +16,7 @@ class ContentTwo extends Component {
       yearTo: 2016,
       showPercents: true,
       showAbsolute: true,
-      amalgamatedData: []
+      allCountriesData: []
     }
   }
 
@@ -33,60 +33,93 @@ class ContentTwo extends Component {
 
 
   fetchData() {
-    for(let i = 0; i < this.state.countries.length; i++) {
-      const country = this.state.countries[i]
-      console.log(`Fetching data for: ${country}..`)
+    this.setState({allCountriesData: []})
 
-      const populationPromise = this.fetchPopulation(country)
-      const consumptionPromise = this.fetchConsumption(country)
-      const percentCleanPromise = this.fetchPercentClean(country)//wind & solar
-      const percentDirtyPromise = this.fetchPercentDirty(country)//oil, gas & coal
-      const percentNuclearPromise = this.fetchPercentNuclear(country)
-      const percentHydroPromise = this.fetchPercentHydro(country)
-      const promises = [
-        populationPromise,
-        consumptionPromise,
-        percentCleanPromise,
-        percentDirtyPromise,
-        percentNuclearPromise,
-        percentHydroPromise
-      ]
+    const promises = this.state.countries.map((country) => {
+      return this.fetchCountryData(country)
+    })
 
-      Promise
-        .all(promises)
-        .then(([
-          popData,
-          consumptionData,
-          percentCleanData,
-          percentDirtyData,
-          percentNuclearData,
-          percentHydroData
-        ]) => {
-          this.amalgamateDatas(
-            popData,
-            consumptionData,
-            percentCleanData,
-            percentDirtyData,
-            percentNuclearData,
-            percentHydroData)
-        })
-    }
+    Promise
+      .all(promises)
+      .then((allCountriesData) => {
+        for(let i = 0; i < allCountriesData.length; i++) {
+          this.setState(prevState => {
+            return {allCountriesData: [...prevState.allCountriesData, allCountriesData[i]]}
+          })
+        }
+      })
+      .then(() => console.log(this.state.allCountriesData))
   }
 
 
-  amalgamateDatas(popData, consumptionData, percentCleanData, precentDirtyData, percentNuclearData, percentHydroData) {
-    console.log('Population:')
-    console.log(popData)
-    console.log('Energy Consumption, kwh Per Capita:')
-    console.log(consumptionData)
-    console.log('Percentage Energy Produced By Renenewables, Excluding Hydro:')
-    console.log(percentCleanData)
-    console.log('Percentage Energy Produced By Oil, Gas & Coal:')
-    console.log(precentDirtyData)
-    console.log('Percentage Energy Produced By Nuclear:')
-    console.log(percentNuclearData)
-    console.log('Percentage Energy Produced By Hydro:')
-    console.log(percentHydroData)
+  fetchCountryData(country) {
+    console.log(`Fetching data for: ${country}..`)
+
+    const populationPromise = this.fetchPopulation(country)
+    const consumptionPromise = this.fetchConsumption(country)
+    const percentCleanPromise = this.fetchPercentClean(country)//wind & solar
+    const percentDirtyPromise = this.fetchPercentDirty(country)//oil, gas & coal
+    const percentNuclearPromise = this.fetchPercentNuclear(country)
+    const percentHydroPromise = this.fetchPercentHydro(country)
+    const promises = [
+      populationPromise,
+      consumptionPromise,
+      percentCleanPromise,
+      percentDirtyPromise,
+      percentNuclearPromise,
+      percentHydroPromise
+    ]
+
+    return new Promise((resolve, reject) => {
+      Promise
+      .all(promises)
+      .then(([popData, consumptionData, percentCleanData, percentDirtyData, percentNuclearData, percentHydroData]) => {
+        return this.amalgamateData(popData, consumptionData, percentCleanData, percentDirtyData, percentNuclearData, percentHydroData)
+      })
+      .then((countryData) => {resolve(countryData)})
+      .catch((err) => reject(err))
+    })
+  }
+
+
+  amalgamateData(popData, consumptionData, percentCleanData, percentDirtyData, percentNuclearData, percentHydroData) {
+    const amalgamatedData = popData.map((el, ind) => {
+      let countryId = el.country.id
+      let countryName = el.country.value
+      let year = el.year
+      let population = el.population
+      let consumption = consumptionData[ind].consumptionPerCapita
+      let percentClean = percentCleanData[ind].percentClean
+      let percentDirty = percentDirtyData[ind].percentDirty
+      let percentNuclear = percentNuclearData[ind].percentNuclear
+      let percentHydro = percentHydroData[ind].percentHydro
+      let totalPercentEnergy = percentClean + percentDirty + percentNuclear + percentHydro
+      let totalEnergyUsed = population * consumption
+      let totalClean = percentClean * totalEnergyUsed
+      let totalDirty = percentDirty * totalEnergyUsed
+      let totalNuclear = percentNuclear * totalEnergyUsed
+      let totalHydro = percentHydro * totalEnergyUsed
+
+      return {
+        countryId: countryId,
+        countryName: countryName,
+        year: year,
+        population: population,
+        consumptionPerCapita: consumption,
+        percentClean: percentClean,
+        percentDirty: percentDirty,
+        percentNuclear: percentNuclear,
+        percentHydro: percentHydro,
+        totalPercent: totalPercentEnergy,
+        totalClean: totalClean,
+        totalDirty: totalDirty,
+        totalNuclear: totalNuclear,
+        totalHydro: totalHydro,
+        total: totalEnergyUsed
+      }
+    })
+
+    return amalgamatedData
   }
 
 
