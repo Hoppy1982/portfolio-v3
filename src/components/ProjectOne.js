80d623d317837c2657ff90f6bff5cf7d5a74d373
mark hopcraft <mark82hopcraft@gmail.com>
2018-06-09 15:45:11 +0100

    improved responsiveness of projectOne and made break point mobile first

--- src/components/ProjectOne.js
+++ src/components/ProjectOne.js
@@ -52,7 +52,7 @@ class ProjectOne extends Component {
     let desiredWidth
 
     if(window.innerWidth <= 600) {
-      desiredWidth  =  window.innerWidth * 0.8
+      desiredWidth  =  window.innerWidth * 0.9
     }
 
     if(window.innerWidth > 600) {
@@ -69,40 +69,50 @@ class ProjectOne extends Component {
 
   render() {
     return(
-      <div>
-        <div className='contentContainer'>
-          <h2 className='projectHeading' >Particle Alphabet</h2>
-          <div className='controls'>
-            <div>
-              <span className='control'>Type in the box:</span>
-              <input type="text" value={this.state.value} onChange={this.contentOneHandleTargetWordChange} className='control'></input>
+      <div className='projectContainer'>
+        <h2 className='projectHeading'>Particle Alphabet</h2>
+
+        <div className='projectLayout'>
+          <div className='canvasAndDescription'>
+            <div className='canvasAndControlsContainer'>
+              <div className='canvasWrapper'>
+                <ParticleAlphabetOne
+                  canvasWidth={this.state.contentOneCanvasWidth}
+                  canvasHeight={this.state.contentOneCanvasHeight}
+                  targetWord={this.state.contentOneTargetWord}
+                  renderWaypoints={this.state.contentOneRenderWaypoints}
+                  renderPaths={this.state.contentOneRenderPaths}
+                />
+              </div>
+              <div className='controls'>
+                <div>
+                  <span className='control'>Type in the box:</span>
+                  <input type="text" value={this.state.value} onChange={this.contentOneHandleTargetWordChange} className='control'></input>
+                </div>
+                <div>
+                  <button onClick={this.contentOneHandleToggleWP} className='control'>Toggle Waypoints</button>
+                  <button onClick={this.contentOneHandleTogglePaths} className='control'>Toggle Paths</button>
+                </div>
+              </div>
             </div>
-            <div>
-              <button onClick={this.contentOneHandleToggleWP} className='control'>Toggle Waypoints</button>
-              <button onClick={this.contentOneHandleTogglePaths} className='control'>Toggle Paths</button>
-            </div>
+
+            <article className='projectDescription'>
+              <h4 className='articleHeading'>Project Desciption</h4>
+              <p>
+                I made this in vanilla javascript without using any libraries other
+                than React with the idea that it would be good practice at using
+                classes to organise the different particle behaviours. Originally
+                I thought I could use it as full page navigation system. I was going
+                to have the particles form the chosen nav link then all zoom off down
+                a wormhole. They'd then zoom out of wormhole on the destination
+                page and do something else like make an animated pomodoro timer.
+                In the end though I settled on leaving it as a self contained project.
+              </p>
+            </article>
           </div>
-          <div className='canvasComponentWrapper'>
-            <ParticleAlphabetOne
-              canvasWidth={this.state.contentOneCanvasWidth}
-              canvasHeight={this.state.contentOneCanvasHeight}
-              targetWord={this.state.contentOneTargetWord}
-              renderWaypoints={this.state.contentOneRenderWaypoints}
-              renderPaths={this.state.contentOneRenderPaths}
-            />
-          </div>
-          <div className='contentDescription'>
-            <p>
-              I made this in plain old javascript without using any libraries other
-              than React with the idea that it would be good practice at using
-              classes to organise the different particle behaviours. Originally
-              I thought I could use it as full page navigation system. I was going
-              to have the particles form the chosen nav link then all zoom off down
-              a wormhole. They'd then zoom out of wormhole on the destination
-              page and do something else like make an animated pomodoro timer.
-              In the end though I settled on leaving it as a self contained project.
-            </p>
-            <h4>How It Works</h4>
+
+          <article className='howItWorks'>
+            <h4 className='articleHeading'>How It Works</h4>
             <p>
               There are 3 particle classes, a super Particle class and then sub
               classes for HoldPatternParticle & LettersParticle. The 2 particle
@@ -124,14 +134,20 @@ class ProjectOne extends Component {
               is dependent on the longest word provided. Letter particles are given
               target vertex to fly to.
             </p>
+          </article>
+
+          <div className='stuffUsed'>
             <h4 className='stuffUsed__heading'>Stuff Used</h4>
             <ul className='stuffUsed__list'>
+              <li>Html</li>
+              <li>Css</li>
               <li>Javascript</li>
               <li>Canvas</li>
               <li>React</li>
             </ul>
           </div>
         </div>
+
       </div>
     )
   }
