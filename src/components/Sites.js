4cae3821268335b657049c5ec5060afa248a7c34
mark hopcraft <mark82hopcraft@gmail.com>
2018-08-02 16:03:37 +0100

    got first link working on sites page

--- src/components/Sites.js
+++ src/components/Sites.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react'
+import {
+  Link
+} from 'react-router-dom'
 import './sites.css'
-
+import thumbnailCodingCef from '../images/site-coding-reference.png'
 
 class Sites extends Component {
   render() {
     return(
-      <section>
-        <p>PLACEHOLDER FOR ABOUT COMPONENT</p>
+      <section className='ext-sites-all'>
+
+        <div className='ext-site'>
+          <img className='ext-site__img' src={thumbnailCodingCef}></img>
+          <p className='ext-site__desc'>
+            <strong>Coding Reference:- </strong>
+            Site I'm working on to keep a record of things I've learnt
+            and as a reference for things I often use.
+          </p>
+          <a className='ext-site__link' href='https://hoppy1982.github.io/learning-record/'>..visit site..</a>
+        </div>
+
+
       </section>
     )
   }
 }
+
 
 
 export default Sites
